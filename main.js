21643bcd73fd7c081764b32a362a67a01239cb38
Yomi Kang <yk0498@knapheide.com>
2022-04-11 15:50:44 -0500

    JS: Navbar menu toggle + project UI tweak

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.addEventListener('scroll', () => {
 });
 
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -21,9 +22,16 @@ navbarMenu.addEventListener('click', (event) => {
     if(link==null){
         return;
     }
-
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
 })
+
+//Navbar toggle button for small screen
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+    navbarMenu.classList.toggle('open');
+})
+
 
 // Handle click on "contact me" button on home
 const homeContactBtn = document.querySelector('.home__contact');
@@ -85,8 +93,7 @@ workBtnContainer.addEventListener('click', (e) => {
 // Remove selection from the previous item and select the new on 
 const active = document.querySelector('.category__btn.selected')
 active.classList.remove('selected');
-const target = 
-    e.target.nodeName === 'BUTTON' ? e.target: e.target.parentNode;
+const target = e.target.nodeName === 'BUTTON' ? e.target: e.target.parentNode;
 target.classList.add('selected');
 
 function scrollIntoView(selector){
